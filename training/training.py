8e16b94a37ff4a9d386876adcc60dcdf1209b22d
DieterMieter <dieterdumieter@web.de>
2022-11-08 18:41:26 +0000

    after Hyperdrive

--- training/training.py
+++ training/training.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 
 import azureml.core
@@ -24,6 +24,31 @@ def main():
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations")
 
     args = parser.parse_args()
+    
+    run = Run.get_context()
+
+    if run.identity.startswith('OfflineRun'):
+        ws = Workspace.from_config()
+
+        experiment_name = 'heartfailure-exp'
+        experiment = Experiment(ws, experiment_name)
+
+        interactive_run = experiment.start_logging()
+    else:
+        ws = run.experiment.workspace
+
+
+    ds = Dataset.get_by_name(ws, name='heartfailure-dds')
+
+
+    x_df = ds.to_pandas_dataframe().dropna()
+    y_df = x_df.pop("DEATH_EVENT")
+
+    x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.2, stratify=y_df, random_state=223)
+    x_train.reset_index(inplace=True, drop=True)
+    x_test.reset_index(inplace=True, drop=True)
+    y_train.reset_index(inplace=True, drop=True)
+    y_test.reset_index(inplace=True, drop=True)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
     
@@ -39,31 +64,6 @@ def main():
 
     model_file_name = 'heart_failure_hyperdrive.pkl'
     joblib.dump(value=model, filename=os.path.join(OUTPUT_DIR, model_file_name))
-
-    run = Run.get_context()
-
-    if run.identity.startswith('OfflineRun'):
-        ws = Workspace.from_config()
-
-        experiment_name = 'heart-failure-clinical-data'
-        experiment = Experiment(ws, experiment_name)
-
-        interactive_run = experiment.start_logging()
-    else:
-        ws = run.experiment.workspace
-
-
-    ds = Dataset.get_by_name(ws, name='heartfailure-dds')
-
-
-    x_df = data.to_pandas_dataframe().dropna()
-    y_df = x_df.pop("DEATH_EVENT")
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=223)
-    x_train.reset_index(inplace=True, drop=True)
-    x_test.reset_index(inplace=True, drop=True)
-    y_train.reset_index(inplace=True, drop=True)
-    y_test.reset_index(inplace=True, drop=True)
 
 if __name__ == '__main__':
     main()
